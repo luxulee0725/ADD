ae8a227a3b244115d418d2871dc46a134839ddde
luxulee0725 <62320500+luxulee0725@users.noreply.github.com>
2021-10-19 01:31:34 +0900

    Update sketch_2.js

--- sketch_2.js
+++ sketch_2.js
@@ -8,8 +8,8 @@ function draw() {
   noStroke();
   for (let i = 0; i < height; i += 20) {
     fill(129, 206, 15);
-    rect(0, i, width, 10);
+    rect(0, i, mouseX, 10);
     fill(255);
-    rect(i, 0, 10, height);
+    rect(i, 0, 10, mouseY);
   }
 }
